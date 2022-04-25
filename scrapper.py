efbb96d2b30bc7972620cda2f76de4eb0063f4db
unknown <muxsidov@gmial.com>
2022-04-25 17:17:17 +0500

    Insert the page

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,6 @@
+from asyncio.windows_events import NULL
 from os import link
+from matplotlib.pyplot import title
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -6,6 +8,9 @@ import psycopg2
 from psycopg2 import OperationalError
 
 links_for_parsing = []
+
+# List of job descriptions
+vacacies_list = []
 
 # Parsing all vacancies page (list of vacancies)
 def parse_vacancies(request):
@@ -19,25 +24,41 @@ def parse_vacancies(request):
 def parse_vacancy_view(link):
     vacancy_view = BeautifulSoup(link.content, 'html5lib')
     
+    single_vacancy = []
     # Useful to extract Job_title and Employer
     #print(vacancy_view.title.string)
     
+    # Job title
+    title = vacancy_view.find(class_=re.compile("h2_grey"))
+    single_vacancy.append(title.text)
+    #print(title.text)
+
+    # Employer
+    employer = vacancy_view.find('a', href=re.compile(r"/company_view"))
+    single_vacancy.append(employer.text)
+    #print(employer.text)
+
     # Period of publication
     #print(vacancy_view.find(text="Period of publication"))
 
-    for i in vacancy_view.find_all(class_=re.compile("td_grey")):
+    #for i in vacancy_view.find_all(class_=re.compile("td_grey")):
         #print(i)
-        print(i.text)
+        #print(i.text)
     
     # Search by CSS class td_sfera (td_grey_10 might be usefull too)
     temp = 0
     for i in vacancy_view.find_all(class_=re.compile("td_sfera")):
-        print(i.text)
+        single_vacancy.append(i.text)
+        #print(i.text)
         temp += 1
     
+    if temp < 13:
+        single_vacancy.insert(11, None)
     # if temp > 12:
-    #     # print(temp, vacancy_view.title)
+    #     print(temp, vacancy_view.title)
     #     print(vacancy_view.find_all(class_=re.compile("td_sfera")))
+    print(len(single_vacancy))
+    vacacies_list.append(tuple(single_vacancy))
 
 parse_vacancies(requests.get("https://uzjobs.uz/e/vakansy.html"))
 parse_vacancies(requests.get("https://uzjobs.uz/e/vakansy-2.html"))
@@ -49,7 +70,7 @@ print(len(links_for_parsing))
 #request = requests.get("https://uzjobs.uz/r/vakansy_view-27452.html")
 # for i in links_for_parsing:
 #     parse_vacancy_view(requests.get("https://uzjobs.uz/" + i))
-parse_vacancy_view(requests.get("https://uzjobs.uz/e/vakansy_view-27493.html"))
+parse_vacancy_view(requests.get("https://uzjobs.uz/e/vakansy_view-27529.html"))
 
 # Creating connection to PostgreSQL
 def create_connection(db_name, db_user, db_password, db_host, db_port):
@@ -118,3 +139,14 @@ CREATE TABLE IF NOT EXISTS vacancies (
 """
 
 execute_query(connection, create_users_table)
+
+vacancy_records = ', '.join(["%s"] * len(vacacies_list))
+
+# Inserting into "vacancies db"
+insert_query = (
+    f"INSERT INTO vacancies (job_title, employer, publication_period, position, duties, age, gender, residence, education, requirements, region, employment, salary, motivation, information) VALUES {vacancy_records} RETURNING id" 
+)
+
+connection.autocommit = True
+cursor = connection.cursor()
+cursor.execute(insert_query, vacacies_list)
